21febdcdc427875a1152f1b0e03f3a375ab87e32
JR Lherm <lherm.jr@gmail.com>
2021-01-11 18:50:23 +0100

    enable update of task & mark as done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,18 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.save
+    # raise
+    redirect_to task_path(@task), notice: "Task updated"
+  end
+
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
@@ -28,18 +40,17 @@ class TasksController < ApplicationController
     redirect_to tasks_path, notice: "Task deleted"
   end
 
-  def mark_as_done
+  def finished
     @task = Task.find(params[:id])
     @task.finished = true
     @task.save
 
     redirect_to tasks_path, notice: "Task completed"
-
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :description, :deadline)
+    params.require(:task).permit(:title, :description, :deadline, :finished, :priority)
   end
 end
