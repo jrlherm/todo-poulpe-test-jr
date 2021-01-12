37dafea731cfca80d0826238372b450488dae733
JR Lherm <lherm.jr@gmail.com>
2021-01-12 16:10:53 +0100

    allow user to view, add, and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,40 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
 
+  def new
+    @comment = Comment.new(task_id: params[:task_id])
+  end
+
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = Comment.new(comment_params)
+    @comment.task = @task
+    authorize @comment
+
+    if @comment.save
+      flash[:success] = "Comment successfully added"
+      redirect_to task_path(@task)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.find(params[:id])
+    authorize @comment
+    @comment.destroy
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.permit(:content)
+  end
 end
