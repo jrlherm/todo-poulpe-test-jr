ac7fe71981d0cd494d52223c870adb7187742676
JR Lherm <lherm.jr@gmail.com>
2021-01-13 11:11:06 +0100

    allow user to add a new task from index with a simplified form

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { maximum: 42 }
-  validates :priority, presence: true
+  # validates :priority, presence: true
 end
