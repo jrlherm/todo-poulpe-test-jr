0e008524adc2ae545dfa7613900f2e01c29e5330
JR Lherm <lherm.jr@gmail.com>
2021-01-14 10:48:26 +0100

    not persistant but working

---
+++ app/javascript/controllers/drag_controller.js
@@ -0,0 +1,23 @@
+// Visit The Stimulus Handbook for more details
+// https://stimulusjs.org/handbook/introduction
+//
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+import Rails from "@rails/ujs"
+import { Controller } from "stimulus"
+import Sortable from "sortablejs"
+
+export default class extends Controller {
+  connect() {
+    this.sortable = Sortable.create(this.element, {
+      onEnd: this.end.bind(this)
+    })
+  }
+
+  end(event) {
+    console.log(event);
+  }
+}
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { maximum: 42 }
   # validates :priority, presence: true
+
+  acts_as_list
 end
